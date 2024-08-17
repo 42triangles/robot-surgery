a1104441aa6dcb158838bce3f26aaa7f8c6aaf9a
Grublady <nucleon.spite_0x@icloud.com>
2024-08-17 18:52:54 -0400

    Better movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,31 @@
 extends CharacterBody2D
 
+@export var movement_scale: float = 100
+@export var friction: float = 50
+@export var max_speed: float = 600
+
+var movement: float
 
 func _physics_process(delta: float) -> void:
 	velocity += get_gravity()
+	
+	var speed_x = abs(velocity.x)
+	if is_on_floor():
+		speed_x -= friction
+	speed_x = clamp(speed_x, 0, max_speed)
+	
+	velocity.x = speed_x * sign(velocity.x)
+	
+	velocity.x += movement * movement_scale
+	
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
-	else:
-		velocity.x = 0
+	if event.is_action("move_left") or event.is_action("move_right"):
+		movement = Input.get_axis("move_left", "move_right")
+		get_viewport().set_input_as_handled()
 	
-	if Input.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("jump"):
 		if is_on_floor():
-			velocity.y = -500
+			velocity.y = -1000
+		get_viewport().set_input_as_handled()
