1ed6edad8f83d69900782cf86ab3678ee7ad4356
Grublady <nucleon.spite_0x@icloud.com>
2024-08-17 19:02:57 -0400

    Refactor player jump constant

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var movement_scale: float = 100
 @export var friction: float = 50
 @export var max_speed: float = 600
+@export var jump_power: float = 2000
 
 var movement: float
 
@@ -27,5 +28,5 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("jump"):
 		if is_on_floor():
-			velocity.y = -1000
+			velocity.y = -1 * jump_power
 		get_viewport().set_input_as_handled()
