0b066d6afcc9beda9aa231b393f2dcff89ab279c
Grublady <nucleon.spite_0x@icloud.com>
2024-08-18 17:48:28 -0400

    Allow custom remote transform to fail silently when unused

--- scripts/custom_remote_transform_2d.gd
+++ scripts/custom_remote_transform_2d.gd
@@ -3,7 +3,12 @@ extends Node2D
 @export var relative_scale: float = 1
 
 @onready var target: Node2D = $"/root/World/Scale0Container/Scale0Viewport/Scale0/Environment"
-@onready var offset: Vector2 = target.position - relative_scale * global_position
+
+var offset: Vector2
+
+func _ready() -> void:
+	if is_instance_valid(target):
+		offset = target.position - relative_scale * global_position
 
 func _process(delta: float) -> void:
 	if is_instance_valid(target):
