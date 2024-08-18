882b6af6db517b612f560001d8349e8fe20ef157
Grublady <nucleon.spite_0x@icloud.com>
2024-08-17 23:48:19 -0400

    Refactor to let ScaleFocusManager handle calling fade animations

--- scripts/scale_focus_manager.gd
+++ scripts/scale_focus_manager.gd
@@ -1,14 +1,20 @@
 extends Node
 
 @export var scale_layers: Array[Node] = []
+@export var scale_containers: Array[Node] = []
 
 var current_focus: int = 1:
 	set(new_value):
 		if new_value != current_focus:
 			var scale_increasing = (new_value > current_focus)
-			scale_layers[current_focus].unfocus(scale_increasing)
+			
+			scale_layers[current_focus].unfocused.emit()
+			scale_containers[current_focus].fade(false, scale_increasing)
+			
 			current_focus = new_value
-			scale_layers[current_focus].focus(scale_increasing)
+			
+			scale_layers[current_focus].focused.emit()
+			scale_containers[current_focus].fade(true, scale_increasing)
 
 func _ready() -> void:
 	for scale_layer in scale_layers:
--- scripts/scale_layer.gd
+++ scripts/scale_layer.gd
@@ -2,11 +2,3 @@ extends Node2D
 
 signal focused
 signal unfocused
-
-func focus(scale_increasing: bool) -> void:
-	focused.emit()
-	$"../..".fade(true, scale_increasing)
-	
-func unfocus(scale_increasing: bool) -> void:
-	unfocused.emit()
-	$"../..".fade(false, scale_increasing)
