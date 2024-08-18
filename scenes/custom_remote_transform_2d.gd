ad2b49e1ffb9b276a25e935cd5afc080ad8b29d9
Grublady <nucleon.spite_0x@icloud.com>
2024-08-18 17:45:19 -0400

    Simplify custom remote transform script

--- scenes/custom_remote_transform_2d.gd
+++ scenes/custom_remote_transform_2d.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-@export var target: Node2D = null
 @export var relative_scale: float = 1
 
-var last_target: Node2D = null
-var offset: Vector2 = Vector2.ZERO
+@onready var target: Node2D = $"/root/World/Scale0Container/Scale0Viewport/Scale0/Environment"
+@onready var offset: Vector2 = target.position - relative_scale * global_position
 
 func _process(delta: float) -> void:
-	if target != null:
-		if target != last_target:
-			last_target = target
-			offset = target.position - relative_scale * global_position
+	if is_instance_valid(target):
 		target.global_rotation = global_rotation
 		target.global_position = offset + relative_scale * global_position
-		#target.transform = Transform2D(rotation, offset + relative_scale * position)
