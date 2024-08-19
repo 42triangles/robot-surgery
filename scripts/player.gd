0ab94f87270d936c1b4804a8b9dad8b8a6ac9629
Grublady <nucleon.spite_0x@icloud.com>
2024-08-19 16:16:49 -0400

    Implement remaining WADL movement animations, add import files for tube bot sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,19 @@ extends CharacterBody2D
 @export var jump_power: float = 2000
 @export_custom(PROPERTY_HINT_NONE, "suffix:°") var move_tip: float = 0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var movement: float = 0
+var facing_left: bool = false:
+	set(new_value):
+		if new_value != facing_left:
+			facing_left = new_value
+			sprite.flip_h = facing_left
+			sprite.play_backwards("rotate")
+
+func _ready() -> void:
+	sprite.animation_looped.connect(_on_sprite_animation_looped)
+	sprite.animation_finished.connect(_on_sprite_animation_finished)
 
 func _process(delta: float) -> void:
 	velocity += get_gravity()
@@ -28,12 +40,33 @@ func _process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("move_left") or event.is_action("move_right"):
 		movement = Input.get_axis("move_left", "move_right")
+		
+		if movement > 0:
+			facing_left = false
+		elif movement < 0:
+			facing_left = true
+		
+		if movement != 0 and sprite.animation == "default":
+			sprite.play("walk")
 		get_viewport().set_input_as_handled()
 	
 	if event.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = -1 * jump_power
+			if sprite.animation == "jump":
+				sprite.stop()
+			sprite.play("jump")
 		get_viewport().set_input_as_handled()
 
 func _on_scale_layer_unfocused() -> void:
 	movement = 0
+
+func _on_sprite_animation_finished() -> void:
+	if movement != 0:
+		sprite.play("walk")
+	else:
+		sprite.play("default")
+
+func _on_sprite_animation_looped() -> void:
+	if sprite.animation == "walk" and movement == 0:
+		sprite.play("default")
