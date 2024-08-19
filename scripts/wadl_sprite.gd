b5b067d9e7558a9055c2fc09ab6d88645b516120
Grublady <nucleon.spite_0x@icloud.com>
2024-08-19 18:50:03 -0400

    Fix WADL footsteps not playing on immediately repeated jumps

--- scripts/wadl_sprite.gd
+++ scripts/wadl_sprite.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 func _on_frame_changed() -> void:
 	if animation == "walk" and frame in walk_sfx_frames:
 		steps_audio.play()
+	elif frame == 0 and animation == "jump":
+		steps_audio.play()
 
 func _on_animation_changed() -> void:
 	if animation == "jump" or animation == "rotate":
