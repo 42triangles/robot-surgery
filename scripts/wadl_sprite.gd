2946fe64f87faa9d889557e33c74ed9c021bee7b
Grublady <nucleon.spite_0x@icloud.com>
2024-08-19 18:43:51 -0400

    Add WADL steps sfx

---
+++ scripts/wadl_sprite.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+@export var walk_sfx_frames: Array[int] = []
+
+@onready var steps_audio: AudioStreamPlayer = $StepsAudio
+
+func _ready() -> void:
+	frame_changed.connect(_on_frame_changed)
+	animation_changed.connect(_on_animation_changed)
+
+func _on_frame_changed() -> void:
+	if animation == "walk" and frame in walk_sfx_frames:
+		steps_audio.play()
+
+func _on_animation_changed() -> void:
+	if animation == "jump" or animation == "rotate":
+		steps_audio.play()
