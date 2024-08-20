b6e0b321e6dbf33c2fb64403d52fce795bce46ca
Grublady <nucleon.spite_0x@icloud.com>
2024-08-20 01:53:25 -0400

    Update launchpad with animation and cleaner script

--- scenes/launchpad.gd
+++ scenes/launchpad.gd
@@ -1,25 +1,21 @@
 extends Area2D
 
 @export var velocity: Vector2 = 3000 * Vector2.UP
+@export var cooldown: float = 1
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var cooldown_remaining: float = 0
 
 var entered: Array[PhysicsBody2D] = []
 
-func launch(object: PhysicsBody2D) -> void:
-	if entered.has(object):
-		return
+func _process(delta: float) -> void:
+	if cooldown_remaining > 0:
+		cooldown_remaining -= delta
 
-	if object is CharacterBody2D:
-		object.velocity += velocity
-	elif object is RigidBody2D:
-		object.linear_velocity += velocity
-	
-	entered.append(object)
-
-func try_launch(object: Node2D) -> void:
-	# TODO: There has to be a better way of handling this.... but as is, new types can just be added here
-	if object is Player:
-		launch(object)
-
-func launch_done(body: Node2D) -> void:
-	if body is PhysicsBody2D:
-		entered.erase(body)
+func _on_body_entered(body: Node2D) -> void:
+	if cooldown_remaining <= 0:
+		if body is Player:
+			body.velocity += velocity
+			sprite.play("activate")
+			cooldown_remaining = cooldown
